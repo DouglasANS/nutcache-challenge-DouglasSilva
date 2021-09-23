5b8425e96ece7b279ae7b5f82f79c592634d123c
douglasans <douglasans@hotmail.com>
2021-09-22 21:06:24 -0300

    delete previously employee button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import CreateEmployeePopupComponent from "../components/CreateEmployeePopupComponent";
 import CardEmployeeComponent from "../components/CardEmployeeComponent";
+import UpdateEmployeePopupComponent from "../components/UpdateEmployeePopupComponent";
+import DeleteEmployeePopupComponent from "../components/DeleteEmployeePopupComponent";
 
 export default function Home() {
   let crudCrudEndPoint = process.env.REACT_APP_CRUDCRUD_ENDPOINT;
@@ -16,18 +18,37 @@ export default function Home() {
   const [startDateCreate, setStartDateCreate] = useState("");
   const [teamCreate, setTeamCreate] = useState(null);
 
+  const [popupUpdatePreviously, setPopupUpdatePreviously] = useState('');
+  const [popupDeletePreviously, setPopupDeletePreviously] = useState('');
+
+  const [dataPreviouslyEmployee, setDataPreviouslyEmployeey] = useState('');
+  
   const [allEmployee, setAllEmployee] = useState([]);
 
   //let regexEmail = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
 
+   
+
   useEffect(() => {
     findAllEmployee();
-  });
+  }, []);
 
   async function findAllEmployee() {
-    const {data} = await Axios.get(`https://crudcrud.com/api/${crudCrudEndPoint}/user`)
-    setAllEmployee(data)
-    console.log(data)
+    const { data } = await Axios.get(
+      `https://crudcrud.com/api/${crudCrudEndPoint}/user`
+    );
+    setAllEmployee(data);
+    let excludeNumber = data.length - 1;
+    setDataPreviouslyEmployeey(data[excludeNumber]);
+    console.log("data: ", data);
+  }
+
+  async function deletePreviouslyRegisteredEmployee() {
+    setPopupDeletePreviously(true)
+    //await Axios.delete( `https://crudcrud.com/api/${crudCrudEndPoint}/user/${id}`);
+  }
+
+  function updatePreviouslyRegisteredEmployee() {
   }
 
   async function AddEmployee() {
@@ -57,36 +78,40 @@ export default function Home() {
     setPopupCreate(true);
   }
 
-  function consolea() {
-    console.log(emailCreate);
-    console.log(teamCreate);
+  function teste(){
+    console.log(dataPreviouslyEmployee)
   }
 
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={consolea}>Console</button>
-
+      <button onClick={teste}>
+        teste
+      </button>
+      <button onClick={deletePreviouslyRegisteredEmployee}>
+        Delete Previously Registered Employee
+      </button>
+      <button onClick={updatePreviouslyRegisteredEmployee}>
+      Update Previously Registered Employee
+      </button>
       <button onClick={createEmployee}>Create Employee</button>
-
-      <button onClick={findAllEmployee}>GET</button>
 
       {allEmployee.map((val) => {
         return (
           <div key={val.Name}>
-              <CardEmployeeComponent
-                Employee={{
-                  id: val._id,
-                  name: val.Name,
-                  birthDate: val.BirthDate,
-                  gender: val.Gender,
-                  email: val.Email,
-                  cpf: val.Cpf,
-                  startDate: val.StartDate,
-                  team: val.Team,
-                }}
-              />
-            </div>
+            <CardEmployeeComponent
+              Employee={{
+                id: val._id,
+                name: val.Name,
+                birthDate: val.BirthDate,
+                gender: val.Gender,
+                email: val.Email,
+                cpf: val.Cpf,
+                startDate: val.StartDate,
+                team: val.Team,
+              }}
+            />
+          </div>
         );
       })}
 
@@ -103,6 +128,11 @@ export default function Home() {
           AddEmployee={AddEmployee}
         />
       )}
+
+{popupDeletePreviously && (
+  <DeleteEmployeePopupComponent id={dataPreviouslyEmployee._id} setPopupDelete={setPopupDeletePreviously} />
+      )}
+
     </div>
   );
 }
